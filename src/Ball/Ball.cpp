0dcb2828fa624232e37caef62bfb4db8da52b8d7
TheScientist466 <swarnim.das.01@gmail.com>
2022-02-22 13:24:14 +0530

    mistake in Ball.cpp is corrected

--- src/Ball/Ball.cpp
+++ src/Ball/Ball.cpp
@@ -1,11 +1,21 @@
 #include "Ball.hpp"
 #include "../window/windowConfig.hpp"
 
-Ball::Ball(float radius = 0.f, sf::Vector2f initDir)
+Ball::Ball(float radius, sf::Vector2f initDir)
 {
     shape = sf::CircleShape(radius);
     shape.setOrigin(radius, radius);
     shape.setPosition(config::windowSize.x / 2, config::windowSize.y / 2);
 
     velocityDir = initDir;
+}
+
+sf::Shape* Ball::getShape()
+{
+    return &shape;
+}
+
+void Ball::Move()
+{
+    shape.move(velocityDir * ballSpeed);
 }
